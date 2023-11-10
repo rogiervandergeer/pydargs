03474e2c85244d62a6243cfe3d03685919b59350
Guus <guus.verstegen@ah.nl>
2023-11-10 16:57:40 +0100

    Add support for nested configuration

--- src/pydargs/__init__.py
+++ src/pydargs/__init__.py
@@ -1,10 +1,11 @@
 import argparse
 import sys
 from argparse import _ArgumentGroup, ArgumentParser
-from dataclasses import fields, MISSING
+from dataclasses import fields, MISSING, is_dataclass
 from datetime import date, datetime
 from functools import partial
 from typing import (
+    Any,
     Type,
     TypeVar,
     Optional,
@@ -15,7 +16,7 @@ from typing import (
     get_args,
     Sequence,
     Union,
-), Union
+)
 
 
 class ADataclass(Protocol):
@@ -29,25 +30,38 @@ Dataclass = TypeVar("Dataclass", bound=ADataclass)
 def parse(tp: Type[Dataclass], args: Optional[list[str]] = None) -> Dataclass:
     if args is None:
         args = sys.argv
-    namespace = _create_parser(tp).parse_args(args)
-    return tp(**namespace.__dict__)
+    parsed_args = _create_parser(tp).parse_args(args).__dict__
+    return create_recursive_object(tp, parsed_args)
+
+
+def create_recursive_object(tp: Type[Dataclass], parsed_args: dict[str, Any], prefix: str = "") -> Dataclass:
+    args = {}
+    for field in fields(tp):
+        if is_dataclass(field.type):
+            args[field.name] = create_recursive_object(field.type, parsed_args, prefix=f"{prefix}{field.name}-")  # type: ignore
+        elif f"{prefix}{field.name}" in parsed_args:
+            args[field.name] = parsed_args[f"{prefix}{field.name}"]
+    return tp(**args)
 
 
 def _create_parser(tp: Type[Dataclass]) -> ArgumentParser:
     parser = ArgumentParser()
-    add_arguments(tp, parser)
+    _add_arguments(tp, parser)
     return parser
 
-def add_arguments(tp: Type[Dataclass], parser: Union[ArgumentParser, _ArgumentGroup]):
+
+def _add_arguments(tp: Type[Dataclass], parser: Union[ArgumentParser, _ArgumentGroup], prefix: str = ""):
     for field in fields(tp):
+        if is_dataclass(field.type):
+            _add_arguments(field.type, parser.add_argument_group(field.name), prefix=f"{prefix}{field.name}-")  # type: ignore
         if origin := get_origin(field.type):
             if origin is Sequence or origin is list:
                 if field.default is MISSING and field.default_factory is MISSING:
                     raise NotImplementedError(f"Parsing {origin} without a default is not supported.")
                 parser.add_argument(
-                    f"--{field.name.replace('_', '-')}",
+                    f"--{prefix}{field.name.replace('_', '-')}",
                     default=argparse.SUPPRESS,
-                    dest=field.name,
+                    dest=f"{prefix}{field.name}",
                     help=f"Override field {field.name}.",
                     nargs="*",
                     type=get_args(field.type)[0],
@@ -56,9 +70,9 @@ def add_arguments(tp: Type[Dataclass], parser: Union[ArgumentParser, _ArgumentGr
                 raise NotImplementedError(f"Parsing into type {origin} is not implemented.")
         elif field.type in (date, datetime):
             parser.add_argument(
-                f"--{field.name.replace('_', '-')}",
+                f"--{prefix}{field.name.replace('_', '-')}",
                 default=argparse.SUPPRESS,
-                dest=field.name,
+                dest=f"{prefix}{field.name}",
                 help=f"Override field {field.name}.",
                 required=field.default is MISSING and field.default_factory is MISSING,
                 type=partial(
@@ -69,14 +83,14 @@ def add_arguments(tp: Type[Dataclass], parser: Union[ArgumentParser, _ArgumentGr
             if as_flags := field.metadata.get("as_flags", None):
                 if as_flags:
                     parser.add_argument(
-                        f"--{field.name.replace('_', '-')}",
-                        dest=field.name,
+                        f"--{prefix}{field.name.replace('_', '-')}",
+                        dest=f"{prefix}{field.name}",
                         help=f"Set {field.name} to True.",
                         action="store_true",
                     )
                     parser.add_argument(
-                        f"--no-{field.name.replace('_', '-')}",
-                        dest=field.name,
+                        f"--no-{prefix}{field.name.replace('_', '-')}",
+                        dest=f"{prefix}{field.name}",
                         help=f"Set {field.name} to False.",
                         action="store_false",
                     )
@@ -84,20 +98,21 @@ def add_arguments(tp: Type[Dataclass], parser: Union[ArgumentParser, _ArgumentGr
                     raise ValueError(f"Misspecified bool_arg_type: {as_flags}.")
             else:
                 parser.add_argument(
-                    f"--{field.name.replace('_', '-')}",
+                    f"--{prefix}{field.name.replace('_', '-')}",
                     default=field.default,
-                    dest=field.name,
+                    dest=f"{prefix}{field.name}",
                     help=f"Override field {field.name}.",
                     type=_parse_bool,
                 )
         else:
             parser.add_argument(
-                f"--{field.name.replace('_', '-')}",
+                f"--{prefix}{field.name.replace('_', '-')}",
                 default=field.default,
-                dest=field.name,
+                dest=f"{prefix}{field.name}",
                 help=f"Override field {field.name}.",
                 type=field.type,
             )
+
 
 def _parse_bool(arg: str) -> bool:
     if arg.lower() == "true" or arg == "1":
@@ -113,4 +128,3 @@ def _parse_datetime(date_string: str, is_date: bool, date_format: Optional[str] 
 
 
 __all__ = ["parse"]
-
---
+++ tests/test_nested.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, field
+
+from pydargs import parse
+
+@dataclass
+class TConfigLevel3:
+    d: list[str] = field(default_factory=lambda: ['hi', 'under_score'])
+
+@dataclass
+class TConfigLevel2:
+    level_3: TConfigLevel3
+    c: int = 5
+    b: int = 4
+
+@dataclass
+class TConfigLevel1:
+    level_2: TConfigLevel2
+    b: int
+    
+@dataclass
+class TConfigLevel0:
+    level1: TConfigLevel1
+    a: int = 3
+
+class TestNested:
+    def test_nested(self):
+        config = parse(TConfigLevel0, ["--a", "12", "--level1-b", "13", "--level1-level_2-c", "20"])
+        assert config.a == 12
+        assert config.level1.b == 13
+        assert config.level1.level_2.b == 4
+        assert config.level1.level_2.c == 20
+
+"""
+TODO:
+
+add underscores in names
+test nested (double, tripple)
+defaults and required
+underscores naam van classes en fields en in string value of list fields
+"""
