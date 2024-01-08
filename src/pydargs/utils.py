77dc8965177191f766a0795cd26703d5b18ba39d
Rogier van der Geer <rogier@vander-geer.nl>
2024-01-08 08:33:06 +0100

    Clean up type function renaming (#48)
    * Clean up type function renaming
    
    Co-authored-by: Anton S <antonsteenvoorden@gmail.com>

---
+++ src/pydargs/utils.py
@@ -0,0 +1,20 @@
+from functools import partial
+from typing import Any, Callable, TypeVar
+
+Fct = TypeVar("Fct")
+
+
+def named_partial(func: Callable[..., Any], *, _display_name: str, **kwargs) -> Callable[[str], Any]:
+    # Wrapper around partial to give it a name, for argparse to provide meaningful messages
+    result = partial(func, **kwargs)
+    setattr(result, "__name__", _display_name)
+    return result
+
+
+def rename(name: str) -> Callable[[Fct], Fct]:
+    # Function decorator to give a function a specific name, for argparse to provide meaningful messages
+    def wrapper(f: Fct) -> Fct:
+        setattr(f, "__name__", name)
+        return f
+
+    return wrapper
